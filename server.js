413032858c620ec84c8c0c2f9dc1d46e89832385
CelesteVignardi <Vignardi_celeste@hotmail.com>
2021-10-18 20:51:37 -0300

    cambios

--- server.js
+++ server.js
@@ -6,9 +6,25 @@ const db = require('./models')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.get('/', (req, res) => {
-  res.send('Hola Mundo pepe!');
+  res.send('Hola Mundo !');
 });
 
 app.get('/pupil', async (req, res) => {
@@ -44,6 +60,21 @@ app.post('/login', async (req, res) => {
     }
 });
 
+app.put('/pupil', async (req, res) => {
+    try {
+        const { pupil, id } = req.body
+
+        await db.Pupil.update(pupil, {
+            where: {
+                id: id
+            }
+        })
+        res.send({message: 'update ok'});
+    } catch (error) {
+        console.log(error)
+    }
+});
+
 app.listen(PORT, () => {
-  console.log(`Aplicación pepe, escuchando el puerto${PORT}`);
+  console.log(`Aplicación ISDLA, escuchando el puerto${PORT}`);
 });
